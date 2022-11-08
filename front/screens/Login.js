57517a1d3f5e711de3d81d87b34a3fa4ebac78b2
jairoalperez <jairoalberto98@hotmail.com>
2022-11-08 01:18:45 -0500

    localStorage implementado

--- front/screens/Login.js
+++ front/screens/Login.js
@@ -1,80 +1,105 @@
 import React from 'react'
-import {View, StyleSheet, Text, TouchableOpacity, TextInput, Alert} from "react-native";
+import { View, StyleSheet, Text, TouchableOpacity, TextInput, Alert } from "react-native";
 import { useNavigation } from "@react-navigation/native";
 import { useState, useEffect } from "react";
 import axios from 'axios';
 
 const Login = () => {
 
-    useEffect(() => {
-        console.log('cargo login')
-    }, [])
+  useEffect(() => {
+    console.log('cargo login')
+  }, [])
 
-    const navigation = useNavigation();
+  const navigation = useNavigation();
 
-    //Se declara el state log con los parametros del username y password que recibiran de los text input
-    const [log, setLog] = useState({
-        username: '',
-        pass: '',
-    })
+  //Se declara el state log con los parametros del username y password que recibiran de los text input
+  const [log, setLog] = useState({
+    username: '',
+    pass: '',
+  })
 
-    //Se utiliza para setear los parametros del state log
-    const handleChangeText = (username, value) => {
-        setLog({...log, [username]: value})
+  //Se utiliza para setear los parametros del state log
+  const handleChangeText = (username, value) => {
+    setLog({ ...log, [username]: value })
+  }
+
+  //HTTP Login
+  const loginapp = () => {
+    const fetchlogin = async () => {
+      const res = await axios.post('https://backend-mangaread.herokuapp.com/login', {
+        username: log.username,
+        password: log.pass
+      },
+        console.log('Conexion Satisfactoria'),
+      )
+      console.log(res.data)
+      if(res.data == 1) {
+        Alert.alert('Login Satisfactorio')
+        console.log('Login Satisfactorio')
+        getuser()
+      }else {
+        Alert.alert('Datos Incorrectos')
+        console.log('Datos Incorrectos')
       }
-
-    //HTTP Login
-    const loginapp = () => {
-      const fetchlogin = async () => {
-        const res = await axios.post('https://backend-mangaread.herokuapp.com/login', {
-          username: log.username,
-          password: log.pass
-        },
-        console.log('Conexion Satisfactoria')
-        )
-        console.log(res.data)
-      }
-      fetchlogin()
+      
     }
+    fetchlogin()
+  }
+
+  //HTTP Get User By Username
+  const getuser = () => {
+    const fetchgetuser = async () => {
+      const res = await axios.get('https://backend-mangaread.herokuapp.com/buscar-nombre/' + log.username)
+          const datos = res.data
+          const result = datos.find(item => item.username === log.username)
+          localStorage.setItem('username', result.username)
+          localStorage.setItem('userId', result.id_usuario)
+    }
+    fetchgetuser()
+  }
 
   return (
     <View style={styles.container}>
 
-            <Text style={styles.title}>
-                INICIAR SESION
-            </Text>
+      <Text style={styles.title}>
+        INICIAR SESION
+      </Text>
 
-            <TextInput
-            style={styles.tinputu} 
-            keyboardType='default'
-            placeholder='Username'
-            placeholderTextColor= 'gray'
-            onChangeText={(value) => handleChangeText('username', value)}/>
+      <TextInput
+        style={styles.tinputu}
+        keyboardType='default'
+        placeholder='Username'
+        placeholderTextColor='gray'
+        onChangeText={(value) => handleChangeText('username', value)} />
 
-            <TextInput
-            secureTextEntry={true} 
-            style={styles.tinputp}
-            keyboardType='default'
-            placeholder='Contraseña'
-            placeholderTextColor= 'gray'
-            onChangeText={(value) => handleChangeText('pass', value)}/>
+      <TextInput
+        secureTextEntry={true}
+        style={styles.tinputp}
+        keyboardType='default'
+        placeholder='Contraseña'
+        placeholderTextColor='gray'
+        onChangeText={(value) => handleChangeText('pass', value)} />
 
-            <TouchableOpacity
-                onPress={() => {
+      <TouchableOpacity
+        onPress={() => {
 
-                    loginapp()
+          loginapp()
+          
 
-                  
 
-                }}
-                style={styles.button}>
-                    <Text style={styles.textbutton}>
-                    Iniciar Sesion
-                    </Text>
-            </TouchableOpacity>
 
-        </View>
-    )
+
+
+
+        }}
+        style={styles.button}>
+        <Text style={styles.textbutton}>
+          Iniciar Sesion
+        </Text>
+      </TouchableOpacity>
+
+    </View>
+  )
 };
 
 export default Login
@@ -82,58 +107,58 @@ export default Login
 
 /*---------------------------------------------------------------------------------------
 ------------------------------------- Estilos -------------------------------------------
----------------------------------------------------------------------------------------*/ 
+---------------------------------------------------------------------------------------*/
 const styles = StyleSheet.create({
 
-    container: {
-        flex: 1,
-        justifyContent: "center",
-        alignItems: "center",
-        backgroundColor: "gold",
-    
-      },
-      title: {
-        fontSize: 40,
-        marginBottom: 50,
-        fontWeight: 'bold',
-        color: 'darkgoldenrod'
-    
-      },
-      tinputu: {
-        height: 40,
-        marginTop: 80,
-        padding: 10,
-        height: 50, 
-        width: 250,
-        borderRadius: 40,
-        backgroundColor: "white",
+  container: {
+    flex: 1,
+    justifyContent: "center",
+    alignItems: "center",
+    backgroundColor: "gold",
 
-      },
-      tinputp: {
-        height: 40,
-        marginTop: 12,
-        padding: 10,
-        height: 50, 
-        width: 250,
-        borderRadius: 40,
-        backgroundColor: "white"
-        
-      },
-      button: {
-        backgroundColor: "darkgoldenrod",
-        padding: 10,
-        marginTop: 30,
-        borderRadius: 40,
-        justifyContent: "center",
-        alignItems: "center",
-        height: 50, 
-        width: 150,
-      
-      },
-      textbutton: {
-        fontSize: 20,
-        color: "white",
-        
-      },
+  },
+  title: {
+    fontSize: 40,
+    marginBottom: 50,
+    fontWeight: 'bold',
+    color: 'darkgoldenrod'
+
+  },
+  tinputu: {
+    height: 40,
+    marginTop: 80,
+    padding: 10,
+    height: 50,
+    width: 250,
+    borderRadius: 40,
+    backgroundColor: "white",
+
+  },
+  tinputp: {
+    height: 40,
+    marginTop: 12,
+    padding: 10,
+    height: 50,
+    width: 250,
+    borderRadius: 40,
+    backgroundColor: "white"
+
+  },
+  button: {
+    backgroundColor: "darkgoldenrod",
+    padding: 10,
+    marginTop: 30,
+    borderRadius: 40,
+    justifyContent: "center",
+    alignItems: "center",
+    height: 50,
+    width: 150,
+
+  },
+  textbutton: {
+    fontSize: 20,
+    color: "white",
+
+  },
 
 });
